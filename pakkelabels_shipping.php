da04a7de2de7bc1536c0480d4239868c55bb78d2
MathiasReker <mathias@reker.dk>
2019-08-22 21:05:58 +0200

    fix version check

--- pakkelabels_shipping.php
+++ pakkelabels_shipping.php
@@ -37,8 +37,8 @@ class Pakkelabels_Shipping extends CarrierModule
         $this->name = 'pakkelabels_shipping';
         $this->tab = 'shipping_logistics';
         $this->version = '1.3.0';
-        $this->v16 = _PS_VERSION_ >= '1.6.0.0';
-        $this->v17 = _PS_VERSION_ >= '1.7.0.0';
+        $this->v16 = _PS_VERSION_ < '1.7';
+        $this->v17 = _PS_VERSION_ >= '1.7';
         $this->author = 'Pakkelabels.dk';
         $this->need_instance = 0;
         $this->ps_versions_compliancy = ['min' => '1.6', 'max' => _PS_VERSION_];
