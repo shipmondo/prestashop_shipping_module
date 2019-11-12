aab98337298e1b909b6f5a001109927a5b6d979a
MathiasReker <mathias@reker.dk>
2019-11-12 12:19:59 +0100

    fix license format

--- views/js/module/onepagecheckoutps.js
+++ views/js/module/onepagecheckoutps.js
@@ -1,5 +1,5 @@
-/*
- @author    Shipmondo
- @copyright 2019 Shipmondo
- @license   All rights reserved
+/**
+ *  @author    Shipmondo
+ *  @copyright 2019 Shipmondo
+ *  @license   All rights reserved
  */
