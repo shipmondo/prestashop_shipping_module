617f27a0a9bf4da2c7749efc711a519ad359c794
David Petersen <hotdkp@gmail.com>
2021-08-16 09:57:45 +0200

    Solved issues with Supercheckout 7.x

--- views/js/module/supercheckout.js
+++ views/js/module/supercheckout.js
@@ -1,7 +1,27 @@
 /**
  *  @author    Shipmondo
- *  @copyright 2019 Shipmondo
+ *  @copyright 2021 Shipmondo
  *  @license   All rights reserved
- * 
- *  Empty file to overwrite old JS file when updating module.
+ *
  */
+
+
+jQuery(document).ready(function ($) {
+    var supercheckout_selector = '#supercheckout-fieldset';
+
+    window.Shipmondo = {
+        deliveryOptionInputContainerSelector: '#shipping-method',
+        deliveryOptionRowSelector: '.highlight'
+    };
+
+    $(supercheckout_selector).bind("DOMNodeInserted", function (e) {
+        var textNode = e.target;
+        if ($(textNode).is(window.Shipmondo.deliveryOptionInputContainerSelector)) {
+            var current_radio = $('.supercheckout_shipping_option:checked');
+            if (current_radio.val()) {
+                //init click when there are preselected shipping methods
+                current_radio.trigger('click');
+            }
+        }
+    });
+});
