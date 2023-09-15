05500e5d6fc5deaacb7c0ea148fa8e2594eda6c2
Jan Petersen <jp@novitas.dk>
2023-09-15 15:36:50 +0200

    old pakkelabels module

--- views/js/module/thecheckout.js
+++
@@ -1,8 +0,0 @@
-/**
- *  @author    Shipmondo
- *  @copyright 2023 Shipmondo
- *  @license   All rights reserved
- */
-$('body').on('change', '.live', function () {
-    $('.delivery-option input:checked').trigger('click');
-});
