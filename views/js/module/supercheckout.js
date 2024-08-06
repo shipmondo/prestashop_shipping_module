173659e0753da6bc7e4a15a1ff8b650145904605
Tezting <hotdkp@gmail.com>
2024-08-06 14:38:52 +0200

    DOM event fix

--- views/js/module/supercheckout.js
+++ views/js/module/supercheckout.js
@@ -1,26 +1,33 @@
 /**
  *  @author    Shipmondo
- *  @copyright 2023 Shipmondo
+ *  @copyright 2024 Shipmondo
  *  @license   All rights reserved
  *
  */
 
 jQuery(document).ready(function ($) {
-    var supercheckout_selector = '#supercheckout-fieldset';
+    const supercheckout_selector = '#supercheckout-fieldset';
 
     window.Shipmondo = {
         deliveryOptionInputContainerSelector: '#shipping-method',
         deliveryOptionRowSelector: '.highlight'
     };
 
-    $(supercheckout_selector).bind("DOMNodeInserted", function (e) {
-        var textNode = e.target;
-        if ($(textNode).is(window.Shipmondo.deliveryOptionInputContainerSelector)) {
-            var current_radio = $('.supercheckout_shipping_option:checked');
-            if (current_radio.val()) {
-                // init click when there are preselected shipping methods
-                current_radio.trigger('click');
-            }
-        }
-    });
+    const supercheckout_element = $(supercheckout_selector);
+    if (supercheckout_element && supercheckout_element.length === 1) {
+        const observer = new MutationObserver(mutationList =>
+            mutationList.filter(m => m.type === 'childList').forEach(m => {
+                m.addedNodes.forEach(function (textNode) {
+                    if ($(textNode).is(window.Shipmondo.deliveryOptionInputContainerSelector)) {
+                        const current_radio = $('.supercheckout_shipping_option:checked');
+                        if (current_radio.val()) {
+                            // init click when there are preselected shipping methods
+                            current_radio.trigger('click');
+                        }
+                    }
+                });
+
+            }));
+        observer.observe(supercheckout_element[0], {childList: true, subtree: true});
+    }
 });
