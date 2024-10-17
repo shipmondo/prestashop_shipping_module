e635996fac1506d5a4da95398a5d8b93975395e3
Jan Petersen <jp@novitas.dk>
2024-10-17 14:16:27 +0200

    Translations

---
+++ translations/sv-SE/index.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ *  @author    Shipmondo <support@shipmondo.com>
+ *  @copyright 2024-present Shipmondo
+ *  @license   https://opensource.org/license/bsd-3-clause BSD-3-Clause
+ */
+header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
+header('Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT');
+
+header('Cache-Control: no-store, no-cache, must-revalidate');
+header('Cache-Control: post-check=0, pre-check=0', false);
+header('Pragma: no-cache');
+
+header('Location: ../');
+exit;
