98cd24fdf2fe8edbad3d971bef8e309acdbd0e4f
Jan Petersen <jp@novitas.dk>
2024-10-24 09:55:16 +0200

    remove dockerfile just run composer via docker image

--- build.sh
+++ build.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
 
-docker build -t shipmondo-prestashop-builder .
-docker run --name build-shipmondo-prestashop shipmondo-prestashop-builder
-docker cp build-shipmondo-prestashop:/app/shipmondo.zip ./shipmondo.zip
-docker rm build-shipmondo-prestashop
+rm shipmondo.zip
+
+cp -rf . shipmondo
+
+cd shipmondo
+rm -rf .git
+rm -rf .vscode
+rm -rf .php-cs-fixer.cache
+rm -rf .php-cs-fixer.dist.php
+rm -rf build.sh
+rm -rf composer.lock
+
+shopt -s extglob
+cd vendor
+rm -rf !(.htaccess)
+cd ..
+
+docker run --rm -v "$(pwd)":/app -w /app composer install --no-dev
+
+cd ..
+zip -r shipmondo.zip shipmondo
+
+rm -rf shipmondo
